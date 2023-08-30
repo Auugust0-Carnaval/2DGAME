8f6e4dc9158ccfcd8788dbcd508d9b999760be11
Augusto <augustooliveixa@gmail.com>
2023-08-30 01:14:23 -0300

    Adding attributes and data method for entering parameters

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,11 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    //propriedades do Player
+
+    public int maxHealth = 100; // essa sera a vida maxima do player
+    public int currentHealth; // vidal atual do Player
+
+
     public float speed; // valor é daclarado no unitycine 
     public float direction; // setado com valor de input e vetores do RigidBody
     public Rigidbody2D rig;
@@ -16,20 +23,32 @@ public class Player : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     public Animator animator;
 
-    //dashing
+    //Dashing
     public float dashSpeed;
     public float dashDuration;
     private bool isDashing = false; // valor de dash começa com falso | sem ter feito algum dashing
     private float dashEndTime;
     private float dashCooldownEndTime;
 
+    //exibir vida
+
+    public TextMeshProUGUI healthText; // refeencia ao componente Text
+
 
     void Start()
     {
+        currentHealth = maxHealth; // setando valor inicial da vida do jogador (100)
         spriteRenderer = GetComponent<SpriteRenderer>();
 
         animator = GetComponent<Animator>();
 
+        healthText = GameObject.Find("text").GetComponent<TextMeshProUGUI>(); // busca o gameObject pelo Find() e seta na variavel
+
+    }
+
+    void UpdateHealthText()
+    {
+        healthText.text = "vida" + currentHealth.ToString(); // atualiza o texto com a vaiavel
     }
 
     void Update()
@@ -46,33 +65,21 @@ public class Player : MonoBehaviour
             dashCooldownEndTime = Time.time + 0.8f;
         }
 
-
-
-
-
-
         if (Input.GetAxis("Horizontal") != 0)
         {
             //esta correndo
             animator.SetBool("isRunning", true);
-
-
-
         }
         else
         {
             //esta parado
             animator.SetBool("isRunning", false);
-
         }
-
 
         if (canJump && Input.GetKeyDown(KeyCode.Space))
         {
             rig.AddForce(Vector2.up * jumper, ForceMode2D.Impulse);
             canJump = false;
-
-            //Debug.Log(contJump++);
         }
 
         // Limita a velocidade vertical
@@ -84,13 +91,13 @@ public class Player : MonoBehaviour
         if (direction > 0) // verefica a direção do movimento e vira o jogador
         {
             spriteRenderer.flipX = false; //vira para direita
-
         }
         else if (direction < 0)
         {
             spriteRenderer.flipX = true; //vira para esquerda
-
         }
+
+        //Debug.Log(currentHealth);
     }
 
     void OnCollisionEnter2D(Collision2D collision) // metodo de colisao, adicionando uma tag do GameObject como parametro
@@ -125,6 +132,27 @@ public class Player : MonoBehaviour
             //cooldown do dash terminou, permita o proximo dash/
 
             //posso adioconar logico como efeitos sonoros ou animaçõe visuais
+        } 
+    }
+
+    //funcao para tomar dano
+    public void TakeDamege(int damageAmout)
+    {
+        currentHealth -= damageAmout; //subritai o dano autual coma vida atual do player
+        UpdateHealthText(); //chama o metodo e atualiza  variavel a cada dano tomado
+
+        if(currentHealth <= 0) // se o  hp do personagem for menor ou igaul a 0
+        {
+            Die(); // persnagem morre
+
+             // colocarei a animaçaõ em breve
+
         }
+    }
+
+    void Die()
+    {
+
+
     }
 }
